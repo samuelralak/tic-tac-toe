7ba6e5b59489fac4ad6e0fd8fcb171891b8076fa
Samuel Ralak <thesamuelralak@gmail.com>
2022-12-15 11:37:54 +0300

    build board layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ function App() {
   return (
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-3xl">
-          This is the game
+            <div className="flex flex-col mt-8">
+                <table className="table-fixed text-center">
+                    <tbody className="divide-y divide-gray-200" >
+                    <tr className="divide-x divide-gray-200">
+                        <td className="py-3">x</td>
+                        <td className="py-3">x</td>
+                        <td className="py-3">x</td>
+                    </tr>
+                    <tr className="divide-x divide-gray-200">
+                        <td className="py-3">o</td>
+                        <td className="py-3">o</td>
+                        <td className="py-3">o</td>
+                    </tr>
+                    <tr className="divide-x divide-gray-200">
+                        <td className="py-3">x</td>
+                        <td className="py-3">x</td>
+                        <td className="py-3">x</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
       </div>
   );
