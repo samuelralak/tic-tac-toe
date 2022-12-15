1634f56286d2386c6e3ebaf759c605406eac923c
Samuel Ralak <thesamuelralak@gmail.com>
2022-12-15 11:51:37 +0300

    dynamically create game board

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 function App() {
-  return (
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-3xl">
-            <div className="flex flex-col mt-8">
-                <table className="table-fixed text-center">
-                    <tbody className="divide-y divide-gray-200" >
-                    <tr className="divide-x divide-gray-200">
-                        <td className="py-3">x</td>
-                        <td className="py-3">x</td>
-                        <td className="py-3">x</td>
-                    </tr>
-                    <tr className="divide-x divide-gray-200">
-                        <td className="py-3">o</td>
-                        <td className="py-3">o</td>
-                        <td className="py-3">o</td>
-                    </tr>
-                    <tr className="divide-x divide-gray-200">
-                        <td className="py-3">x</td>
-                        <td className="py-3">x</td>
-                        <td className="py-3">x</td>
-                    </tr>
-                    </tbody>
-                </table>
+    const board = Array.from(Array(3), _ => [...Array(3).fill(false)])
+
+    return (
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl">
+                <div className="flex flex-col mt-8">
+                    <table className="table-fixed text-center">
+                        <tbody className="divide-y divide-gray-200">
+                            {board.map((row, index) => (
+                                <tr key={`row-${index}`} className="divide-x divide-gray-200">
+                                    {row.map((col, idx) => (
+                                        <td key={`col-${index}-${idx}`} className="py-3 cursor-pointer">x</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
